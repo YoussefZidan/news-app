6385cd9330578efae285e9e42d6da0852d64339b
Youssef Zidan <youssefzidan2200@gmail.com>
2020-11-21 11:57:26 +0200

    creating navbar icon and responsive

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Navbar, Nav } from "reactstrap";
+import { Navbar, Nav, Badge } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/imgs/logo.png";
 import avatar from "../assets/imgs/avatar.png";
@@ -27,6 +27,13 @@ const NavBar = () => {
   const style = {
     height: spaces.navBarHeight,
   };
+  const badgeStyle = {
+    height: "15px",
+    width: "5px",
+    position: "absolute",
+    top: "-5px",
+    left: "15px",
+  };
   return (
     <React.Fragment>
       {/* Side Menu */}
@@ -45,9 +52,35 @@ const NavBar = () => {
           <NavLink to="/">
             <img src={logo} alt="Logo" width="60vw" />
           </NavLink>
-          <Nav className="ml-auto" navbar>
-            <NavLink to="/">
-              <img src={avatar} alt="User" width="40vw" />
+          <Nav className="ml-auto align-items-center" navbar>
+            <div className="mr-4">
+              {/* Search */}
+              <NavLink to="/">
+                <Icon.Search className="text-white mr-3" size={30} />
+              </NavLink>
+              {/* Notification */}
+              <NavLink to="/" className="position-relative">
+                <Icon.Bell className="text-white mr-lg-3" size={30} />
+                <Badge
+                  color="success"
+                  pill
+                  className="badge-up"
+                  style={badgeStyle}>
+                  {" "}
+                </Badge>
+              </NavLink>
+            </div>
+            {/* Avatar */}
+            <div className="d-none d-sm-block">
+              <NavLink to="/" className="mr-4 d-flex align-items-center">
+                <img src={avatar} alt="User" width="40vw" />
+                <p className="my-0 ml-2 text-white ">User Name</p>
+              </NavLink>
+            </div>
+
+            {/* Settings */}
+            <NavLink to="/" className="d-none d-sm-block">
+              <Icon.Settings className="text-white" size={30} />
             </NavLink>
           </Nav>
         </div>
