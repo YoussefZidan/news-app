13436c8e2c605050d7e1ac81d2d96b830fd0ec3b
Youssef Zidan <youssefzidan2200@gmail.com>
2020-11-22 17:07:11 +0200

    installing redux

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/home";
+import Details from "./pages/details";
 
 const Routes = () => {
   return (
     <Switch>
+      <Route
+        path="/details/:id"
+        render={(props) => <Details {...props}></Details>}
+      />
       <Route path="/home" render={(props) => <Home {...props}></Home>} />
-      <Route path="/blog" render={(props) => <h1 className="text-center p-5">Blog Page</h1>} />
-      <Redirect from="/" exact to="/home" />
+      <Route
+        path="/blog"
+        render={(props) => <h1 className="text-center p-5">Blog Page</h1>}
+      />
+
+      <Redirect from="/" to="/home" />
     </Switch>
   );
 };
