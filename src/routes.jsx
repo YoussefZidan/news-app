a0ed92aa1e0599ef8cc28f5488d7d46e5c43ee6e
Youssef Zidan <youssefzidan2200@gmail.com>
2020-11-26 17:34:35 +0200

    adding HashRouter in index.js

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,17 +6,15 @@ import AllNews from "./pages/allNews";
 
 const Routes = () => {
   return (
-    <HashRouter basename="/">
-      <Switch>
-        <Route
-          path="/details/:id"
-          render={(props) => <Details {...props}></Details>}
-        />
-        <Route path="/home" render={(props) => <Home {...props} />} />
-        <Route path="/news" render={(props) => <AllNews {...props} />} />
-        <Redirect from="/" to="/home" />
-      </Switch>
-    </HashRouter>
+    <Switch>
+      <Route
+        path="/details/:id"
+        render={(props) => <Details {...props}></Details>}
+      />
+      <Route path="/home" render={(props) => <Home {...props} />} />
+      <Route path="/news" render={(props) => <AllNews {...props} />} />
+      <Redirect from="/" to="/home" />
+    </Switch>
   );
 };
 
