3ad73803650557311c621880032c741fecef9ee4
Youssef Zidan <youssefzidan2200@gmail.com>
2020-11-26 17:36:53 +0200

    removing warnings

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/home";
 import Details from "./pages/details";
 import AllNews from "./pages/allNews";
