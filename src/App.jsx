a84f1d4e3100b7b0a1148832b8d8b48494a323ac
Youssef Zidan <youssefzidan2200@gmail.com>
2020-11-27 16:34:44 +0200

    reorganize folders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Routes from "./routes";
 import "./App.scss";
 import Header from "./components/shared/header/header";
-import Footer from "./components/footer";
+import Footer from "./components/shared/footer";
+
+/**
+ * App Component
+ */
 const App = () => {
   return (
     <React.Fragment>
