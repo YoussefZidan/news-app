9d75fc90754548ec641d68c7a2c38d1a703cfe95
Youssef Zidan <youssefzidan2200@gmail.com>
2020-11-28 13:54:29 +0200

    responsive banner polygons

--- src/components/home/banner.jsx
+++ src/components/home/banner.jsx
@@ -2,28 +2,17 @@ import React from "react";
 import Polygon from "../shared/polygon";
 
 const Banner = () => {
-  const headerStyle = {
-    fontSize: "5vw",
-    paddingTop: "2vh",
-  };
-
   return (
-    <div
-      style={{ height: "100vh", backgroundColor: "gray" }}
-      className="banner text-white text-center mx-auto">
-      <div className="mb-5">
-        <h2 className="font-weight-bold mb-4" style={headerStyle}>
-          We Have Helped
-        </h2>
+    <div className="banner text-white text-center mx-auto">
+      <div className="mb-5 text">
+        <h2 className="text-header font-weight-bold mb-4">We Have Helped</h2>
         <p className="w-75 m-auto">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate et
           ut rerum, nulla, dolorem quaerat id ipsum, eius natus cum quo. Nisi
           animi illum consectetur.
         </p>
       </div>
-      <div
-        className="m-auto p-5 d-none d-lg-block mt-5"
-        style={{ width: "80%" }}>
+      <div className="polygons m-auto p-5 mt-5" style={{ width: "80%" }}>
         <div className="d-flex">
           <Polygon />
           <Polygon />
--- src/components/shared/polygon.jsx
+++ src/components/shared/polygon.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import * as Icon from "react-feather";
 const Polygon = () => {
   return (
-    <section className="polygon m-auto">
+    <section className="polygon m-auto pointer">
       <div className="polygon-inner">
-        <Icon.File size={42} className="mb-2" />
+        <Icon.Box size={42} className="mb-2" />
         <h3>Lorem ipsum dolor sit.</h3>
       </div>
     </section>
